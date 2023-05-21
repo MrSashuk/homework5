cc6e1d316acdc1291d004ae9145cbc5f667fa704
MrSashuk <pavlovecsasa04@gmail.com>
2023-05-21 10:05:16 +0300

    add Django Models, gitignore file, admin.py

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import UserListView, HomeView
+
+
+urlpatterns = [
+    path('', HomeView.as_view(), name='home'),
+    path('users/', UserListView.as_view(), name='user'),
+]
